049c97e42349d27d7369f9483c3f93e04fee5c97
openlabocean <tobi@openlab.org.uk>
2014-11-21 19:28:34 +0000

    1st commit

---
+++ HTMLStrip.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+from re import sub
+from sys import argv
+
+def clean_html(html):
+    cleaned = re.sub(r"(?is)<(script|style).*?>.*?(</\1>)", "", html.strip())
+    cleaned = sub(r"(?s)<!--(.*?)-->[\n]?", "", cleaned)
+    cleaned = sub(r"(?s)<.*?>", " ", cleaned)
+    cleaned = sub(r"&nbsp;", " ", cleaned)
+    cleaned = sub(r"  ", " ", cleaned)
+    cleaned = sub(r"  ", " ", cleaned)
+    return cleaned.strip()
+
+def main():
+    try:
+        html_text = open(argv[1]).read()
+        clean_html = clean_html(html_text)
+        print(clean_html)
+
+    except:
+        print("Please provide a valid html file to parse.")
+
+
+
+if __name__ == "__main":
+    main()
